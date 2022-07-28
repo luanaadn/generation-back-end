224747730dce8746fb1ea133583c63d6772e256a
luanaadn <luanaadn@hotmail.com>
2022-07-27 22:26:23 -0300

    Ajuste

--- MySQL/02 - Atividade de banco de dados relacional Mysql/Atividade01.sql
+++ MySQL/02 - Atividade de banco de dados relacional Mysql/Atividade01.sql
@@ -2,22 +2,23 @@ CREATE DATABASE db_colaboradores;
 
 USE db_colaboradores;
 
-CREATE TABLE tb_informacoes(
-id BIGINT AUTO_INCREMENT PRIMARY KEY,
-nome_colaborador VARCHAR(255),
-cargo VARCHAR(255),
-salario DECIMAL(10,8),
-turno VARCHAR(255),
-vale_transporte BOOLEAN
+CREATE TABLE tb_colaboradores(
+	id BIGINT AUTO_INCREMENT PRIMARY KEY,
+	nome VARCHAR(255),
+	cargo VARCHAR(255),
+	salario DECIMAL(10,2),
+	turno VARCHAR(255),
+	vale_transporte BOOLEAN
 );
 
-INSERT INTO tb_informacoes (nome_colaborador, cargo, salario, turno, vale_transporte) VALUES ("Marcos Rodrigues", "Porteiro", 1.500, "Manhã/Tarde", true);
-INSERT INTO tb_informacoes (nome_colaborador, cargo, salario, turno, vale_transporte) VALUES ("Carmelita Lima", "Serviços gerais", 1.325, "Manhã/Tarde", treu);
-INSERT INTO tb_informacoes (nome_colaborador, cargo, salario, turno, vale_transporte) VALUES ("Thiago do Nascimento", "Vigia", 10.500, "Tarde/noite", false);
-INSERT INTO tb_informacoes (nome_colaborador, cargo, salario, turno, vale_transporte) VALUES ("Patricia Diogenes", "Administradora", 4.200, "Manhã/Tarde", false);
-INSERT INTO tb_informacoes (nome_colaborador, cargo, salario, turno, vale_transporte) VALUES ("Tereza de Castro", "Desenvolvedora Júnior", 3.200, "Manhã/Tarde", true);
-INSERT INTO tb_informacoes (nome_colaborador, cargo, salario, turno, vale_transporte) VALUES ("Pedro Eduardo", "Desenvolvedor Sênior", 10.500, "Manhã/Tarde", false);
+INSERT INTO tb_colaboradores (nome, cargo, salario, turno, vale_transporte) VALUES ("Marcos Rodrigues", "Porteiro", 1500, "Manhã/Tarde", True);
+INSERT INTO tb_colaboradores (nome, cargo, salario, turno, vale_transporte) VALUES ("Carmelita Lima", "Serviços gerais", 1325, "Manhã/Tarde", True);
+INSERT INTO tb_colaboradores (nome, cargo, salario, turno, vale_transporte) VALUES ("Thiago do Nascimento", "Vigia", 10500, "Tarde/noite", False);
+INSERT INTO tb_colaboradores (nome, cargo, salario, turno, vale_transporte) VALUES ("Patricia Diogenes", "Administradora", 4200, "Manhã/Tarde", False);
+INSERT INTO tb_colaboradores (nome, cargo, salario, turno, vale_transporte) VALUES ("Tereza de Castro", "Desenvolvedora Júnior", 3200, "Manhã/Tarde", True);
+INSERT INTO tb_colaboradores (nome, cargo, salario, turno, vale_transporte) VALUES ("Pedro Eduardo", "Desenvolvedor Sênior", 10500, "Manhã/Tarde", False);
 
-SELECT * FROM db_colaboradores.tb_informacoes;
+SELECT * FROM db_colaboradores.tb_colaboradores;
 
+UPDATE tb_colaboradores SET nome = "Carmelita Diogenes" WHERE id =2;
 
--- MySQL/02 - Atividade de banco de dados relacional Mysql/Atividade02.sql
+++ MySQL/02 - Atividade de banco de dados relacional Mysql/Atividade02.sql
@@ -7,24 +7,23 @@ id BIGINT AUTO_INCREMENT PRIMARY KEY,    -- Chave primária da tabela. Deixar p/
 nome_produto VARCHAR(255),     			 -- Varchar = String.
 valor DECIMAL(10,2),		   			 -- Aceita 8 números no total, com 2 números após a vírgula.			
 quantidade INT,							 -- Separar o comando por vírgulas.
-pagamento_cartao BOOLEAN
+pagamento_cartao BOOLEAN,
+delivery BOOLEAN
 );
 
-INSERT INTO tb_produtos (Nome_produto, valor, quantidade, pagamento_cartao) VALUES ("Camiseta", 25.59,5, true);      -- Iserir na tabela produtos. Colocar na ordem que criou. Iserir os valores.
-INSERT INTO tb_produtos (Nome_produto, valor, quantidade, pagamento_cartao) VALUES ("Calça", 109.90,3, true);
-INSERT INTO tb_produtos (Nome_produto, valor, quantidade, pagamento_cartao) VALUES ("Vestido", 80.59,2, false);
-INSERT INTO tb_produtos (Nome_produto, valor, quantidade, pagamento_cartao) VALUES ("Bermurda", 70.49,1, false);
-INSERT INTO tb_produtos (Nome_produto, valor, quantidade, pagamento_cartao) VALUES ("Sunga", 60,3, true);
-INSERT INTO tb_produtos (Nome_produto, valor, quantidade, pagamento_cartao) VALUES ("Biquini", 80,3, true);
-INSERT INTO tb_produtos (Nome_produto, valor, quantidade, pagamento_cartao) VALUES ("Cinto", 40.99,2, false);
-INSERT INTO tb_produtos (Nome_produto, valor, quantidade, pagamento_cartao) VALUES ("Sapato social", 149.90,1, true);
+INSERT INTO tb_produtos (Nome_produto, valor, quantidade, pagamento_cartao, delivery) VALUES ("Camiseta", 25.59, 5, true, true);      -- Iserir na tabela produtos. Colocar na ordem que criou. Iserir os valores.
+INSERT INTO tb_produtos (Nome_produto, valor, quantidade, pagamento_cartao, delivery) VALUES ("Calça", 109.90, 3, true, false);
+INSERT INTO tb_produtos (Nome_produto, valor, quantidade, pagamento_cartao, delivery) VALUES ("Vestido", 80.59, 2, false, false);
+INSERT INTO tb_produtos (Nome_produto, valor, quantidade, pagamento_cartao, delivery) VALUES ("Bermurda", 70.49, 1, false, true);
+INSERT INTO tb_produtos (Nome_produto, valor, quantidade, pagamento_cartao, delivery) VALUES ("Sunga", 60, 3, true, true);
+INSERT INTO tb_produtos (Nome_produto, valor, quantidade, pagamento_cartao, delivery) VALUES ("Biquini", 80, 3, true, false);
+INSERT INTO tb_produtos (Nome_produto, valor, quantidade, pagamento_cartao, delivery) VALUES ("Cinto", 40.99, 2, false, true);
+INSERT INTO tb_produtos (Nome_produto, valor, quantidade, pagamento_cartao, delivery) VALUES ("Sapato social", 149.90, 1, true, false);
 
 SELECT * FROM db_lojinha.tb_produtos;
 
 SELECT * FROM tb_produtos WHERE valor > 500;
 
-SELECT * FROM tb_produtos WHERE valor < 500;    -- Não dá erro, mas não aparece.
+SELECT * FROM tb_produtos WHERE valor < 500;    
 
 UPDATE tb_produtos SET Nome_produto = "Camisa social" WHERE id=1;
-
-
